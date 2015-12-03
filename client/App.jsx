07c67f38d004ea629b56bff3387179236e6dd610
marushkevych <marushkevych@gmail.com>
2015-12-03 18:40:35 -0500

    initial commit

---
+++ client/App.jsx
@@ -0,0 +1,80 @@
+// App component - represents the whole app
+App = React.createClass({
+  // This mixin makes the getMeteorData method work
+  mixins: [ReactMeteorData],
+
+  // Loads items from the Tasks collection and puts them on this.data.tasks
+  getMeteorData() {
+    let query = {};
+
+    if (this.state.hideCompleted) {
+      // If hide completed is checked, filter tasks
+      query = {checked: {$ne: true}};
+    }
+
+    return {
+      tasks: Tasks.find(query, {sort: {createdAt: -1}}).fetch(),
+      incompleteCount: Tasks.find({checked: {$ne: true}}).count()
+    }
+  },
+
+  getInitialState() {
+    return {
+      hideCompleted: false
+    }
+  },
+
+  renderTasks() {
+    return this.data.tasks.map((task) => {
+      return <Task key={task._id} task={task} />;
+    });
+  },
+
+  handleSubmit(event) {
+    event.preventDefault();
+    var task = this.refs.textInput.value.trim();
+    if(task){
+      Tasks.insert({
+        text: task,
+        createdAt: new Date()
+      });
+    }
+
+    this.refs.textInput.value = '';
+  },
+
+  toggleHideCompleted() {
+    this.setState({hideCompleted: !this.state.hideCompleted});
+  },
+
+  render() {
+    console.log('App render');
+    return (
+        <div className="container">
+          <header>
+            <h1>Todo List ({this.data.incompleteCount})</h1>
+
+            <label className="hide-completed">
+              <input
+                  type="checkbox"
+                  readOnly={true}
+                  checked={this.state.hideCompleted}
+                  onClick={this.toggleHideCompleted} />
+              Hide Completed Tasks
+            </label>
+
+            <form className="new-task" onSubmit={this.handleSubmit} >
+              <input
+                  type="text"
+                  ref="textInput"
+                  placeholder="Type to add new tasks" />
+            </form>
+          </header>
+
+          <ul>
+            {this.renderTasks()}
+          </ul>
+        </div>
+    );
+  }
+});
